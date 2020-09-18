c8f9ce0b7c89aa113e9871daa9cd40f3f3c8daeb
chenzdido <chenzdido@gmail.com>
2020-09-18 16:58:22 +0800

    [zhe] feat:create branch jpa.

--- demo/src/main/java/com/example/demo/exception/IDNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class IDNotFoundException extends RuntimeException {
-    public  IDNotFoundException(String message) {
-        super(message);
-    }
-}
